8ee4601f49fff64b373d13bbb5c62b8acc1980c0
Robert Czobor <czobor94@gmail.com>
2024-12-14 23:35:52 +0100

    initial commit

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,37 @@
+import { z } from "zod";
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from "@/server/api/trpc";
+import { posts } from "@/server/db/schema";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input, ctx }) => {
+      console.log(ctx.userId);
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  create: protectedProcedure
+    .input(z.object({ name: z.string().min(1) }))
+    .mutation(async ({ ctx, input }) => {
+      console.log(ctx.userId, ctx.auth.userId);
+      await ctx.db.insert(posts).values({
+        createdBy: ctx.userId,
+        name: input.name,
+      });
+    }),
+
+  getLatest: publicProcedure.query(async ({ ctx }) => {
+    const post = await ctx.db.query.posts.findFirst({
+      orderBy: (posts, { desc }) => [desc(posts.createdAt)],
+    });
+
+    return post ?? null;
+  }),
+});
