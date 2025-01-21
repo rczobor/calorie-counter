48b300c1bb9d5713dffa13623f49ce8032ce6ee3
nethzir <czobor94@gmail.com>
2025-01-21 20:56:48 +0100

    format updateAt date in table

--- src/app/cookings/columns.ts
+++ src/app/cookings/columns.ts
@@ -3,5 +3,9 @@ import { type ColumnDef } from "@tanstack/react-table";
 
 export const columns: ColumnDef<Cooking>[] = [
   { accessorKey: "name", header: "Name" },
-  { accessorKey: "updatedAt", header: "Updated At" },
+  {
+    accessorKey: "updatedAt",
+    header: "Updated At",
+    cell: ({ row }) => row.original.updatedAt?.toLocaleString(),
+  },
 ];
