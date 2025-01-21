ebceae3ed97a6e478159fadf82ab0ba04d270b4c
nethzir <czobor94@gmail.com>
2025-01-21 20:56:29 +0100

    remove dashboard and redirect to '/cookings'

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -4,7 +4,6 @@ import { SignedOut, SignInButton, SignedIn, UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "@/components/mode-toggle";
 
 const navItems = [
-  { name: "Dashboard", href: "/" },
   { name: "Ingredients", href: "/ingredients" },
   { name: "Recipes", href: "/recipes" },
   { name: "Cookings", href: "/cookings" },
