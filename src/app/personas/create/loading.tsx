362e9447cefba6b5e60cd0d86c44583f25f64a79
nethzir <czobor94@gmail.com>
2025-04-01 23:30:53 +0200

    add headers for loading skeletons

--- src/app/personas/create/loading.tsx
+++ src/app/personas/create/loading.tsx
@@ -4,6 +4,9 @@ import { columns } from "../columns";
 export default function Loading() {
   return (
     <div className="container mx-auto flex flex-col px-4">
+      <div className="flex items-center justify-between py-4">
+        <h1 className="text-2xl font-bold">Persona</h1>
+      </div>
       <DataTable columns={columns} loading={true} />
     </div>
   );
