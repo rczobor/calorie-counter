76883c6b82686eba0a2b793e02bdb84a4a0e219d
Robert Czobor <robert.czobor@tesco.com>
2026-02-14 17:54:49 +0100

    fix vercel deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import { fileURLToPath, URL } from 'url'
@@ -20,6 +21,7 @@ const config = defineConfig({
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact({
       babel: {
         plugins: ['babel-plugin-react-compiler'],
