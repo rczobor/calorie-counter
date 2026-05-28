886e28414313ab67c324d8c00eb0d24e434f3ffc
rczobor <czobor94@gmail.com>
2026-05-28 22:13:48 +0200

    Add Vercel Analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { Analytics } from '@vercel/analytics/react'
 import {
   ClerkLoaded,
   ClerkLoading,
@@ -98,6 +99,7 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <HeadContent />
       </head>
       <body>
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
